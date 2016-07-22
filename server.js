7092af24897b8aac9653f3e3011bf1e71f06f31d
Jonathon Lo <jonathon.lo35@gmail.com>
2016-07-22 16:29:58 -0700

    Initialize MovableInk Creative

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+	
+  console.log('Example app listening on' + port);
+});
+
